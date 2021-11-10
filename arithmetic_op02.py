0ac56153f0c03abcf150d73b3027b95d690c02e7
Safarov_Yoqubbek <yoqubbeksafarov1997@gmail.com>
2021-11-10 22:21:23 +0300

    03

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,5 +1,8 @@
 #Create a varaible 'number' and assign it the value of 43.
-
+number=43
+number1=number//10
+number2=number%10
 #Create a variable 'answer' and assign it the sum of the number's digits.
-
+answer=number1+number2
 #Print answer.
+print(answer)
--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,9 @@
 #Create a variable called 'number' and assign it the two-digit number
-
-#Find the reverse of the number and assign it to a variable called 'answer'.
-
+number=23
+number1=number//10
+number2=number%10
+number3=number2*10+number1
+#Find the reverse of the number and assign it to a variable called 'answer'
+answer=number3
 #Print the answer variable
+print(answer)
